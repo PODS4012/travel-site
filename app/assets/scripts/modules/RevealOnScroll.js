28b27fafbdd0dac4bac7d533008d8657b5cb5e7b
Darius <pods4012@gmail.com>
2017-04-10 00:05:47 +0200

    done

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,32 +1,31 @@
 import $ from 'jquery';
-import wayponts from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
-	constructor(els, offset) {
-		this.itemsToReveal = els;
-		this.offsetPrecentage = offset;
-		this.hideInitially();
-		this.createWaypoints();
-		
-	}
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.offsetPercentage = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
 
-	hideInitially() {
-		this.itemsToReveal.addClass("reveal-item");
-	}
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
+  }
 
-	createWaypoints() {
-		var that = this;
-		this.itemsToReveal.each(function() {
-			var currentItem = this;
-			new Waypoint({
-				element: currentItem,
-				handler: function() {
-					$(currentItem).addClass("reveal-item--is-visible");
-				},
-				offset: that.offsetPrecentage
-			});
-		});
-	}
+  createWaypoints() {
+    var that = this;
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: that.offsetPercentage
+      });
+    });
+  }
 }
 
 export default RevealOnScroll;
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
-const path = require('path');
-
 module.exports = {
-	entry: "./app/assets/scripts/App.js",
-	output: {
-		path: path.join(__dirname, "./app/temp/scripts"),
-		filename: "App.js"
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'babel-loader', 
-				query: {
-					presets: ['es2015']
-				},
-				test: /\.js$/,
-				exclude: /node_modules/
-			}
-		]
-	}
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
+  output: {
+    path: "./app/temp/scripts",
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
 }
